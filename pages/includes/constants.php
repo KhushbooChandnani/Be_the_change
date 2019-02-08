97e4827d6594a0e21ca8687cfd3c29f06e6a89c4
KhushbooChandnani <37927996+KhushbooChandnani@users.noreply.github.com>
2019-02-08 17:37:41 +0530

    Add files via upload

---
+++ pages/includes/constants.php
@@ -0,0 +1,8 @@
+<?php
+// require_once("status-constants.php");
+define("SERVER","localhost");
+define("USER","Khushboo Chandnani");
+define("PASSWORD","Kitten@21");
+define("DB","ngo");
+define("BASE_SERVER","localhost");
+?>
